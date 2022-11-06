0edfd7b59328e10f57a910e20f6f73f6994b8e37
Valss22 <valsshokorov@gmail.com>
2022-11-06 14:04:31 +0600

    Removed unused comments

--- nonblocking_io/eventloop.py
+++ nonblocking_io/eventloop.py
@@ -1,38 +1,25 @@
 import asyncio
-from curses import wrapper
-from functools import wraps
+from types import FunctionType
 import nest_asyncio
 
 nest_asyncio.apply()
 
 
-# def nonblocking(task, *args):
-#     loop = asyncio.get_event_loop()
-#     task = loop.create_task(task(*args))
-#     loop.run_until_complete(asyncio.wait([task]))
+class EventLoop:
+    def __init__(self):
+        self.__tasks = []
+        self.__loop = asyncio.get_event_loop()
+
+    def nonblocking(self, task, *args):
+        task = self.__loop.create_task(task(*args))
+        self.__tasks.append(task)
+        if len(self.__tasks) >= 0:
+            self.__loop.run_until_complete(asyncio.wait(self.__tasks))
+            self.__tasks.clear()
 
 
-# class EventLoop:
-#     def __init__(self, func=None) -> None:
-#         self.__func = func
-#         self.__tasks = []
-#         self.__loop = asyncio.get_event_loop()
-
-#     async def __call__(self):
-#         self.__tasks.append(self.__func)
-#         await self.__func()
-
-#     def nonblocking(self, task, *args):
-#         task = self.__loop.create_task(task(*args))
-#         self.__tasks.append(task)
-#         if len(self.__tasks) >= 0:
-#             self.__loop.run_until_complete(asyncio.wait(self.__tasks))
-#             self.__tasks.clear()
+def foo():
+    pass
 
 
-def nonblocking(func):
-    def wrapper():
-        print(1)
-        return func()
-
-    return wrapper
+print(type(foo) is FunctionType)
--- nonblocking_io/examples.py
+++ nonblocking_io/examples.py
@@ -12,26 +12,9 @@ async def task_2(msg: str) -> None:
     print(msg)
 
 
-# @EventLoop()
-# async def main():
-#     loop = EventLoop()
-#     loop.nonblocking(task_1, "task_1 is completed")
-#     loop.nonblocking(task_2, "task_2 is completed")
-#     print("main is completed")
-
-
 @nonblocking
 async def main():
     print("main is completed")
 
 
 # asyncio.run(main())
-
-# ioloop = asyncio.get_event_loop()
-# tasks = [
-#     ioloop.create_task(task_1("task_1 is completed")),
-#     ioloop.create_task(task_2("task_2 is completed")),
-#     ioloop.create_task(main()),
-# ]
-# ioloop.run_until_complete(asyncio.wait(tasks))
-# ioloop.close()
